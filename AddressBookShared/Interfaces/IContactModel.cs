31d967e4d7f6fe0faa9008f4a2b3eec999f2fd73
Nils <Nils0009@hotmail.com>
2023-12-21 08:19:38 +0100

    ContactModel and interface created

---
+++ AddressBookShared/Interfaces/IContactModel.cs
@@ -0,0 +1,12 @@
+﻿namespace AddressBookShared.Interfaces;
+
+public interface IContactModel
+{
+    string FirstName { get; set; }
+    string LastName { get; set; }
+    string Email { get; set; }
+    string PhoneNumber { get; set; }
+    string StreetName { get; set; }
+    string City { get; set; }
+    string PostalCode { get; set; }
+}
---
+++ AddressBookShared/Models/ContactModel.cs
@@ -0,0 +1,15 @@
+﻿using AddressBookShared.Interfaces;
+
+namespace AddressBookShared.Models;
+
+
+public class ContactModel : IContactModel
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;    
+    public string StreetName { get; set; } = null!;
+    public string City { get; set; } = null!;
+    public string PostalCode { get; set; } = null!;
+}
