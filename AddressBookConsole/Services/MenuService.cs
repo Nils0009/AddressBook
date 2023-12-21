0c7300c9c878bf3e4026335b213e4a8b71f9302d
Nils <Nils0009@hotmail.com>
2023-12-21 11:19:46 +0100

    menuservice methods under construction

--- AddressBookConsole/Services/MenuService.cs
+++ AddressBookConsole/Services/MenuService.cs
@@ -26,125 +26,155 @@ public class MenuService
             switch (userMenuInput)
             {
                 case 1:
-                    IContactModel contact = new ContactModel();
-
-                    Console.WriteLine("Add contact");
-                    Console.Write("First name: ");
-                    contact.FirstName = Console.ReadLine()!.ToLower();
-                    Console.Write("Last name: ");
-                    contact.LastName = Console.ReadLine()!.ToLower();
-                    Console.Write("Email: ");
-                    contact.Email = Console.ReadLine()!.ToLower();
-                    Console.Write("Phone number: ");
-                    contact.PhoneNumber = Console.ReadLine()!.ToLower();
-                    Console.Write("Street name: ");
-                    contact.StreetName = Console.ReadLine()!.ToLower();
-                    Console.Write("City: ");
-                    contact.City = Console.ReadLine()!.ToLower();
-                    Console.Write("Postal code: ");
-                    contact.PostalCode = Console.ReadLine()!.ToLower();
-
-                    _contactService.AddContactToList(contact);
+                    ShowAddMenu();
                     break;
                 case 2:
-                    Console.WriteLine("Contact list");
-                    _contactService.GetAllContactsFromList();
-                    foreach (ContactModel contact1 in _contactService.GetAllContactsFromList())
-                    {
-                        Console.WriteLine($"{contact1.FirstName}, {contact1.LastName}, {contact1.Email}, {contact1.PhoneNumber}, {contact1.StreetName}, {contact1.City}, {contact1.PostalCode}");
-                    }
+                    ShowContactListMenu();
                     break;
                 case 3:
-                    Console.Write("Update contact with email: ");
-                    string userUpdateEmailInput = Console.ReadLine()!.ToLower();
-                    var contactToBeUpdated = _contactService.GetOneContactByEmail(userUpdateEmailInput);
-                    Console.WriteLine("Contact found");
-                    Console.WriteLine($"{contactToBeUpdated.FirstName}, {contactToBeUpdated.LastName}, {contactToBeUpdated.Email}, {contactToBeUpdated.PhoneNumber}, {contactToBeUpdated.StreetName}, {contactToBeUpdated.City}, {contactToBeUpdated.PostalCode}");
-                    Console.Write("Do you want to update? (y/n): ");
-                    string userUpdateAnswerInput = Console.ReadLine()!.ToLower();
-                    if (userUpdateAnswerInput == "y")
-                    {
-                        Console.Write("First name: ");
-                        string newFirstName = Console.ReadLine()!.ToLower();
-                        contactToBeUpdated.FirstName = newFirstName;
-
-                        Console.Write("Last name: ");
-                        string newLastName = Console.ReadLine()!.ToLower();
-                        contactToBeUpdated.LastName = newLastName;
-
-                        Console.Write("Email: ");
-                        string newEmail = Console.ReadLine()!.ToLower();
-                        contactToBeUpdated.Email = newEmail;
-
-                        Console.Write("Phone number: ");
-                        string newPhoneNumber = Console.ReadLine()!.ToLower();
-                        contactToBeUpdated.PhoneNumber = newPhoneNumber;
-
-                        Console.Write("Street name: ");
-                        string newStreetName = Console.ReadLine()!.ToLower();
-                        contactToBeUpdated.StreetName = newStreetName;
-
-                        Console.Write("City: ");
-                        string newCity = Console.ReadLine()!.ToLower();
-                        contactToBeUpdated.City = newCity;
-
-                        Console.Write("Postal code: ");
-                        string newPostalCode = Console.ReadLine()!.ToLower();
-                        contactToBeUpdated.PostalCode = newPostalCode;
-
-                        _contactService.UpdateContactInList(contactToBeUpdated, newFirstName, newLastName, newEmail, newPhoneNumber, newStreetName, newCity, newPostalCode);
-
-                        Console.WriteLine("Contact was updated");
-                    }
+                    ShowContactUpdateMenu();
                     break;
                 case 4:
-                    Console.WriteLine("Delete contact");
-                    Console.Write("Remove contact with email: ");
-                    string userRemoveInput = Console.ReadLine()!.ToLower();
-                    var userToRemoveFound = _contactService.GetOneContactByEmail(userRemoveInput);
-
-                    if (userToRemoveFound != null)
-                    {
-                        userToRemoveFound.FirstName = Console.ReadLine()!;
-                        userToRemoveFound.LastName = Console.ReadLine()!;
-                        userToRemoveFound.Email = Console.ReadLine()!;
-                        userToRemoveFound.PhoneNumber = Console.ReadLine()!;
-                        userToRemoveFound.StreetName = Console.ReadLine()!;
-                        userToRemoveFound.City = Console.ReadLine()!;
-                        userToRemoveFound.PostalCode = Console.ReadLine()!;
-
-                        Console.Write("Do you want to remove? (y/n): ");
-                        string userRemoveInputAnswer = Console.ReadLine()!;
-
-                        if (userRemoveInputAnswer == "y")
-                        {
-                            _contactService.RemoveContactFromList(userRemoveInput);
-                            Console.WriteLine("Contact was removed");
-                        }
-                    }
-
-
+                    ShowDeleteMenu();
                     break;
                 case 5:
-                    Console.Write("Search contact with email: ");
-                    string userSearchInput = Console.ReadLine()!.ToLower();
-                    if (!string.IsNullOrEmpty(userSearchInput))
-                    {
-                        var userFound = _contactService.GetOneContactByEmail(userSearchInput);
-                        Console.WriteLine("Contact was found");
-                        Console.WriteLine($"{userFound.FirstName}, {userFound.LastName}, {userFound.Email}, {userFound.PhoneNumber}, {userFound.StreetName}, {userFound.City}, {userFound.PostalCode}");
-                    }
+                    ShowContactSearchMenu();
                     break;
                 case 6:
-                    Console.Write("Do you want to exit (y/n)?: ");
-                    string userExitInput = Console.ReadLine()!.ToLower();
-                    if (userExitInput == "y")
-                    {
-                        Environment.Exit(0);
-                    }
+                    ShowExitMenu();
                     break;
             }
 
         }
     }
+    public void ShowAddMenu()
+    {
+        IContactModel contact = new ContactModel();
+
+        Console.WriteLine("Add contact");
+        Console.Write("First name: ");
+        if (!string.IsNullOrEmpty(Console.ReadLine()) && !Console.ReadLine()!.Any(char.IsDigit))
+        {
+            contact.FirstName = Console.ReadLine()!.ToLower();
+        }
+
+        Console.Write("Last name: ");
+        contact.LastName = Console.ReadLine()!.ToLower();
+        Console.Write("Email: ");
+        contact.Email = Console.ReadLine()!.ToLower();
+        Console.Write("Phone number: ");
+        contact.PhoneNumber = Console.ReadLine()!.ToLower();
+        Console.Write("Street name: ");
+        contact.StreetName = Console.ReadLine()!.ToLower();
+        Console.Write("City: ");
+        contact.City = Console.ReadLine()!.ToLower();
+        Console.Write("Postal code: ");
+        contact.PostalCode = Console.ReadLine()!.ToLower();
+
+        _contactService.AddContactToList(contact);
+    }
+    public void ShowContactListMenu()
+    {
+        Console.WriteLine("Contact list");
+        _contactService.GetAllContactsFromList();
+        foreach (ContactModel contact1 in _contactService.GetAllContactsFromList())
+        {
+            Console.WriteLine($"{contact1.FirstName}, {contact1.LastName}, {contact1.Email}, {contact1.PhoneNumber}, {contact1.StreetName}, {contact1.City}, {contact1.PostalCode}");
+        }
+    }
+    public void ShowContactUpdateMenu() 
+    {
+        Console.Write("Update contact with email: ");
+        string userUpdateEmailInput = Console.ReadLine()!.ToLower();
+        var contactToBeUpdated = _contactService.GetOneContactByEmail(userUpdateEmailInput);
+        Console.WriteLine("Contact found");
+        Console.WriteLine($"{contactToBeUpdated.FirstName}, {contactToBeUpdated.LastName}, {contactToBeUpdated.Email}, {contactToBeUpdated.PhoneNumber}, {contactToBeUpdated.StreetName}, {contactToBeUpdated.City}, {contactToBeUpdated.PostalCode}");
+        Console.Write("Do you want to update? (y/n): ");
+        string userUpdateAnswerInput = Console.ReadLine()!.ToLower();
+        if (userUpdateAnswerInput == "y")
+        {
+            Console.Write("First name: ");
+            string newFirstName = Console.ReadLine()!.ToLower();
+            contactToBeUpdated.FirstName = newFirstName;
+
+            Console.Write("Last name: ");
+            string newLastName = Console.ReadLine()!.ToLower();
+            contactToBeUpdated.LastName = newLastName;
+
+            Console.Write("Email: ");
+            string newEmail = Console.ReadLine()!.ToLower();
+            contactToBeUpdated.Email = newEmail;
+
+            Console.Write("Phone number: ");
+            string newPhoneNumber = Console.ReadLine()!.ToLower();
+            contactToBeUpdated.PhoneNumber = newPhoneNumber;
+
+            Console.Write("Street name: ");
+            string newStreetName = Console.ReadLine()!.ToLower();
+            contactToBeUpdated.StreetName = newStreetName;
+
+            Console.Write("City: ");
+            string newCity = Console.ReadLine()!.ToLower();
+            contactToBeUpdated.City = newCity;
+
+            Console.Write("Postal code: ");
+            string newPostalCode = Console.ReadLine()!.ToLower();
+            contactToBeUpdated.PostalCode = newPostalCode;
+
+            _contactService.UpdateContactInList(contactToBeUpdated, newFirstName, newLastName, newEmail, newPhoneNumber, newStreetName, newCity, newPostalCode);
+
+            Console.WriteLine("Contact was updated");
+        }
+    }
+    public void ShowDeleteMenu()
+    {
+        Console.WriteLine("Delete contact");
+        Console.Write("Remove contact with email: ");
+        string userRemoveInput = Console.ReadLine()!.ToLower();
+        var userToRemoveFound = _contactService.GetOneContactByEmail(userRemoveInput);
+
+        if (userToRemoveFound != null)
+        {
+            userToRemoveFound.FirstName = Console.ReadLine()!;
+            userToRemoveFound.LastName = Console.ReadLine()!;
+            userToRemoveFound.Email = Console.ReadLine()!;
+            userToRemoveFound.PhoneNumber = Console.ReadLine()!;
+            userToRemoveFound.StreetName = Console.ReadLine()!;
+            userToRemoveFound.City = Console.ReadLine()!;
+            userToRemoveFound.PostalCode = Console.ReadLine()!;
+
+            Console.Write("Do you want to remove? (y/n): ");
+            string userRemoveInputAnswer = Console.ReadLine()!;
+
+            if (userRemoveInputAnswer == "y")
+            {
+                _contactService.RemoveContactFromList(userRemoveInput);
+                Console.WriteLine("Contact was removed");
+            }
+        }
+
+
+    }
+    public void ShowContactSearchMenu()
+    {
+        Console.Write("Search contact with email: ");
+        string userSearchInput = Console.ReadLine()!.ToLower();
+        if (!string.IsNullOrEmpty(userSearchInput))
+        {
+            var userFound = _contactService.GetOneContactByEmail(userSearchInput);
+            Console.WriteLine("Contact was found");
+            Console.WriteLine($"{userFound.FirstName}, {userFound.LastName}, {userFound.Email}, {userFound.PhoneNumber}, {userFound.StreetName}, {userFound.City}, {userFound.PostalCode}");
+        }
+    }
+    public void ShowExitMenu()
+    {
+        Console.Write("Do you want to exit (y/n)?: ");
+        string userExitInput = Console.ReadLine()!.ToLower();
+        if (userExitInput == "y")
+        {
+            Environment.Exit(0);
+        }
+    }
+
+
 }
