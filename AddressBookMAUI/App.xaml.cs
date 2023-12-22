962e94af1d164cffc2a66dd66a7d6941e2a76b73
Nils <Nils0009@hotmail.com>
2023-12-22 15:32:52 +0100

    MAUI project created and under construction

---
+++ AddressBookMAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AddressBookMAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AddressBookMAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AddressBookMAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
